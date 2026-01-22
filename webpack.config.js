a6e28da2b5beb931570f98dd212e888c2d86be47
Olesya Krayanskaya <krayanskaya_ov@mail.ru>
2026-01-22 20:07:00 +0300

    create an empty project

---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: path.resolve(__dirname, 'src', 'index.ts'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.[contenthash].js',
+        assetModuleFilename: path.join('[name].[contenthash][ext]'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.js$/,
+                use: ['babel-loader'],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    { loader: MiniCssExtractPlugin.loader, options: {} },
+                    {
+                        loader: 'css-loader',
+                        options: { url: true, import: true },
+                    },
+                    { loader: 'postcss-loader' },
+                    { loader: 'sass-loader' },
+                ],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.svg$/i,
+                type: 'asset/inline',
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+            filename: 'index.html',
+            chunks: ['index'],
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+    ],
+    devServer: {
+        host: '0.0.0.0',
+        watchFiles: path.resolve(__dirname, 'src'),
+        port: 9000,
+    },
+    optimization: {
+        minimizer: [
+            new ImageMinimizerPlugin({
+                minimizer: {
+                    implementation: ImageMinimizerPlugin.imageminMinify,
+                    options: {
+                        plugins: [
+                            ['gifsicle', { interlaced: true }],
+                            ['jpegtran', { progressive: true }],
+                            ['optipng', { optimizationLevel: 5 }],
+                            ['svgo', { name: 'preset-default' }],
+                        ],
+                    },
+                },
+            }),
+        ],
+    },
+};
